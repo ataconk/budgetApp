bb67277753fe7ba02ecd2619c59c5f661aeb6cd7
Atacan Korkmaz <atk@marketlogicsoftware.com>
2021-03-17 19:42:32 +0100

    airbnb date picker

--- src/components/expenseForm.js
+++ src/components/expenseForm.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css'
 
 export default class ExpenseForm extends React.Component {
     state ={ 
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        focused: false
     };
     onDescriptionChange = (e) => {
         const description = e.target.value
@@ -19,8 +24,15 @@ export default class ExpenseForm extends React.Component {
     onAmountChange = (e) => {
         const amount = e.target.value
         if(amount.match(/^\d*(\.\d{0,2})?$/)) {
-            this.setState(()=> ({amount}))
+            this.setState(()=> ({amount}));
         }
+    }
+    onDateChange = (createdAt) => {
+        this.setState(()=> ({ createdAt }));
+    }
+
+    onFocusChange = ({ focused } ) => {
+        this.setState(() => ({ focused:focused }));
     }
     render() {
         return (
@@ -39,7 +51,14 @@ export default class ExpenseForm extends React.Component {
                         value = {this.state.amount}
                         onChange={this.onAmountChange}
                     />
-
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange = {this.onDateChange}
+                        focused={this.state.focused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                    />
                     <textarea
                         placeholder='Add a note for your expense (opt)'
                         value={this.state.note}
