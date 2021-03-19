019bb444054e7f2ea4adfe16558ed3f36137760a
Atacan Korkmaz <atacankorkmaz3@gmail.com>
2021-03-19 16:20:40 +0100

    cleaned ops from app.js

--- src/app.js
+++ src/app.js
@@ -10,27 +10,8 @@ import getVisibleExpenses from '../src/selectors/expenses'
 import { setTextFilter } from '../src/actions/filters'
 
 const store =configureStore();
-store.dispatch(addExpense({
-    description: 'Water Bill',
-    amount: 3000,
-    createdAt: 3000
-}))
-
-store.dispatch(addExpense({
-    description: 'Gas Bill',
-    amount: 5000,
-    createdAt: 2500
-}))
-
-store.dispatch(addExpense({
-    description: 'Rent Bill',
-    amount: 109500,
-    createdAt: 7000
-}))
 
 const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
 
 
 const jsx = (
