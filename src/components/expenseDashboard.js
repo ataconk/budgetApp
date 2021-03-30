a75f267902ba6ac4caf278012e749df2200fdd76
Atacan Korkmaz <atacankorkmaz3@gmail.com>
2021-03-30 13:24:14 +0200

    expenses total, and its tests

--- src/components/expenseDashboard.js
+++ src/components/expenseDashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpenseList from './expenseList'
 import ExpenseListFilters from './expenseListFilters'
+import ExpensesSummary from './expensesSummary'
 
 const ExpenseDashboardPage = () => (
     <div>
         <ExpenseListFilters/>
         <ExpenseList />
+        <ExpensesSummary/>
     </div>
 );
 
