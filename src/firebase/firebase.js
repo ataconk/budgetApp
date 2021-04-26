6d340d13b13c7ad969f3f8ba7161ebb8ceb5378e
Atacan Korkmaz <atacankorkmaz3@gmail.com>
2021-04-26 18:16:13 +0200

    seperating test and dev DB

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,12 @@
 import  firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC8m1Z5ecwPHMtC-M73OlOF16SYSrmNroo",
-    authDomain: "budget-app-dc870.firebaseapp.com",
-    databaseURL: "https://budget-app-dc870-default-rtdb.firebaseio.com",
-    projectId: "budget-app-dc870",
-    storageBucket: "budget-app-dc870.appspot.com",
-    messagingSenderId: "236777761622",
-    appId: "1:236777761622:web:5e37ca7a2ae6fe058844f7",
-    measurementId: "G-XW3ZSBLQWQ"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
